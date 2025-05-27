98af85b4dfdc2ec18ed4c97ce8852218d74d018f
shubham-virtualTE <spchaudhari@virtualemployee.com>
2025-05-27 21:57:07 +0530

    updating room-details model

--- models/user/RoomDetails.js
+++ models/user/RoomDetails.js
@@ -1,18 +1,26 @@
 const mongoose = require("mongoose");
-const {Schema}  = require("mongoose");
+const { Schema } = require("mongoose");
 
-const {ObjectId} = Schema.Types
+const { ObjectId } = Schema.Types
 
 const DroppedItemSchema = new mongoose.Schema({
   name: { type: String, required: true },
   imageSrc: { type: String, required: true },
-  height: { type: Number, required: true },  
-  width: { type: Number, required: true },   
+  height: { type: Number, required: true },
+  width: { type: Number, required: true },
+  hinges: { type: Number },
+  handles: { type: Number },
+  drawers: { type: Number },
+  overlap: { type: Number },
+  basePrice: { type: String },
+  minWidth: { type: String },
+  maxWidth: { type: String },
+  cabinateType: { type: String, required: true },
 });
 
 const RoomDetailsSchema = new mongoose.Schema(
   {
-    user_id: {type : ObjectId, ref:"User", required: true },
+    user_id: { type: ObjectId, ref: "User", required: true },
     width: {
       type: Number,
       required: true,
@@ -27,7 +35,7 @@ const RoomDetailsSchema = new mongoose.Schema(
     },
     subdescription: {
       type: String,
-      
+
     },
     notes: { type: Object, default: {} },
     droppedItems: { type: [DroppedItemSchema], required: true, default: [] } // Store dropped items
